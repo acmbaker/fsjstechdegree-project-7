29a4368d149815174723c4da04fefb118234cce6
acmbaker <104640776+acmbaker@users.noreply.github.com>
2022-08-10 10:38:55 +0100

    Test

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    
+    render() {
+        return (
+            <ul>
+                <li><a href='#'>Cats</a></li>
+                <li><a href='#'>Dogs</a></li>
+                <li><a href='#'>Computers</a></li>
+          </ul>
+        );
+    }
+}
+
+export default Nav;
